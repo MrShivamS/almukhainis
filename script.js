68c3c936019f3e1ead2bfe6ce81262d6d0b0169d
Shivam <sharmashivam1918@gmail.com>
2026-03-26 13:58:21 +0530

    Logo arabic removed,cards bg chnage,whatsapp+sheets interigration,responsive

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ function scrollToSection() {
   });
 }
 function toggleMenu() {
-  document.getElementById("navLinks").classList.toggle("active");
+  const nav = document.getElementById("navLinks");
+  const overlay = document.getElementById("overlay");
+
+  nav.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
+document.querySelectorAll("#navLinks a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("navLinks").classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+  });
+});
+document.addEventListener("click", function (e) {
+  const nav = document.getElementById("navLinks");
+  const menuBtn = document.querySelector(".menu-toggle");
+
+  if (
+    nav.classList.contains("active") &&
+    !nav.contains(e.target) &&
+    !menuBtn.contains(e.target)
+  ) {
+    nav.classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+  }
+});
+
 let current = 0;
 const slides = document.querySelectorAll(".slide");
 
@@ -21,25 +45,47 @@ function nextSlide() {
 
 setInterval(nextSlide, 4500);
 
-// Auto Slide (important for professional feel)
-setInterval(nextSlide, 4500);
-
+// Whatsapp + sheets
 const form = document.getElementById("contactForm");
+const loader = document.getElementById("loader");
 
-form.addEventListener("submit", function (e) {
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwSU7itISsZ9zaS_H-RvXvsVRqaE2GryzrqZUziYy43UVUSmsBjIzhfuOePKOU_GV43IQ/exec";
+
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const formData = new FormData(form);
+  // loader show
+  loader.style.display = "flex";
 
-  fetch(
-    "https://script.google.com/macros/s/AKfycbwIyqf2sQ6cVq8GZe9_kYdsTXS4YNrRVChObTFprOrfsgFK8z1ujFD6amYhc0994N_v5g/exec",
-    {
+  let formData = new FormData(form);
+
+  let name = formData.get("name");
+  let email = formData.get("email");
+  let message = formData.get("message");
+
+  try {
+    //  send to Google Sheets
+    await fetch(scriptURL, {
       method: "POST",
-      mode: "no-cors",
-      body: formData, // 🔥 NOT JSON
-    },
-  ).then(() => {
-    alert("Message sent!");
+      body: new URLSearchParams({
+        name: name,
+        email: email,
+        message: message,
+      }),
+    });
+
+    //  WhatsApp message
+    let whatsappMessage = `:%0AName: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+
+    //  open WhatsApp
+    window.open(`https://wa.me/917249853816?text=${whatsappMessage}`, "_blank");
+
     form.reset();
-  });
+  } catch (error) {
+    alert("Something went wrong!");
+  }
+
+  // loader hide
+  loader.style.display = "none";
 });
